2f3fa55d86cc451ae5d5b032ee4441a8644addd7
v4lomyr <bintangdwitama160501@gmail.com>
2025-02-07 05:36:46 +0800

    feat: read user command

--- cmd/shell/main.go
+++ cmd/shell/main.go
@@ -6,8 +6,16 @@ import (
 	"os"
 )
 
+var _ = fmt.Fprint
+
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	cmd, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input:", err)
+		os.Exit(1)
+	}
+
+	fmt.Fprintln(os.Stdout, cmd[:len(cmd)-1]+": command not found")
 }
